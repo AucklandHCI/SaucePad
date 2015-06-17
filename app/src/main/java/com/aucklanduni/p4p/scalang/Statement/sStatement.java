d3f1b9a68393688465b946c9e438b72e87907fc3
Tazt <thus056@aucklanduni.ac.nz>
2015-06-17 19:23:35 +1200

    forgot to add this

---
+++ app/src/main/java/com/aucklanduni/p4p/scalang/Statement/sStatement.java
@@ -0,0 +1,14 @@
+package com.aucklanduni.p4p.scalang.Statement;
+
+import com.aucklanduni.p4p.scalang.KeypadItem;
+import com.aucklanduni.p4p.scalang.ScalaClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Taz on 17/06/15.
+ */
+public abstract class sStatement extends ScalaClass {
+
+}
