5853a5c3ea9c9b10cdca2f538ea4b7c4943924b9
Tazt <thus056@aucklanduni.ac.nz>
2015-08-09 21:50:07 +1200

    forgot to add this file

---
+++ app/src/main/java/com/aucklanduni/p4p/scalang/expression/NullExpr.java
@@ -0,0 +1,20 @@
+package com.aucklanduni.p4p.scalang.expression;
+
+import com.aucklanduni.p4p.scalang.ScalaElement;
+import com.aucklanduni.p4p.scalang.printer.VoidVisitor;
+
+/**
+ * Created by Taz on 2/08/15.
+ */
+public class NullExpr extends ScalaElement implements sExpression   {
+
+    @Override
+    public void accept(VoidVisitor v) {
+        v.visit(this);
+    }
+
+    @Override
+    protected String toPrintAfterDone() {
+        return "";
+    }
+}
